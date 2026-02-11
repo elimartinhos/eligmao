b0afa741504cf8aa73fc0eed22c93e02ee0b582c
elimartinhos <obsta@hotmail.com>
2026-02-11 15:06:43 -0300

    Add files via upload

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,21 @@
+export default async function handler(req, res) {
+    const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+    const { method, body } = req.body;
 
+    try {
+        const response = await fetch(`https://api.github.com/repos/elimartinhos/eligmao/contents/data.json`, {
+            method: method,
+            headers: {
+                "Authorization": `token ${token}`,
+                "Accept": "application/vnd.github.v3+json",
+                "Content-Type": "application/json"
+            },
+            body: body ? JSON.stringify(body) : null
+        });
+
+        const data = await response.json();
+        return res.status(response.status).json(data);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
